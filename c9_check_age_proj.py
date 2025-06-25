5a874ca8cf78e46f7553b00a389c4f4efe573e6c
Vivien-Shah <hfsthane7787@gmail.com>
2025-06-25 19:17:20 +0530

    Add files via upload

---
+++ c9_check_age_proj.py
@@ -0,0 +1,24 @@
+# To check if the users age is between 10 to 20 years or not.
+
+print()
+age=int(input("Enter your age: "))
+if age >=10 and age <=20:
+    print("\nYour age is between 10 and 20\n")
+elif age < 10:
+    print("\n Your age is below 10\n")
+else:
+    print("\nYour age is above 20\n")
+print()
+
+''' # To check if the users age is between 10 to 20 years or not.
+
+print()
+age=int(input("Enter your age: "))
+print()
+if age >=10 and age <=20:
+    print("Your age is between 10 and 20")
+elif age < 10:
+    print("Your age is below 10")
+else:
+    print("Your age is above 20")
+print() '''
